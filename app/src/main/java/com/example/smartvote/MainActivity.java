a6c439a27795783ebab9d6de686453795e616d07
ballzakob <piscis.za@gmail.com>
2018-10-14 14:36:12 +0700

    commit 1

---
+++ app/src/main/java/com/example/smartvote/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.smartvote;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button nextButton = findViewById(R.id.next_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this , Main2Activity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
